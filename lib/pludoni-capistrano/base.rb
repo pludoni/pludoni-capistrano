d12044d189e90c11a0eafe5a302aec597b9514aa
Stefan Wienert <stefan.wienert@pludoni.de>
2012-06-29 19:16:41 +0200

    rvm adjustments acc to http://groups.google.com/group/rubyversionmanager/browse_thread/thread/771df7409797f64b

--- lib/pludoni-capistrano/base.rb
+++ lib/pludoni-capistrano/base.rb
@@ -1,6 +1,9 @@
 require 'bundler/capistrano'
 # Add RVM's lib directory to the load  ...path.
+$:.unshift("#{ENV["HOME"]}/.rvm/lib")
 $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+
+require 'rvm'
 require "rvm/capistrano"                  # Load RVM's capistrano plugin.
 require 'capistrano_colors'
 
