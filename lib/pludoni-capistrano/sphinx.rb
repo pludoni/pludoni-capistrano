61e764437f25f76d678b072676b25c514cdb26c2
Stefan <stefan.wienert@pludoni.de>
2012-09-27 09:16:44 +0200

    sphinx adjustments: always symlink dir

--- lib/pludoni-capistrano/sphinx.rb
+++ lib/pludoni-capistrano/sphinx.rb
@@ -1,15 +1,25 @@
 Capistrano::Configuration.instance.load do
 
-  namespace :deploy do
-    task :new_sphinx, :roles => [:app] do
-      thinking_sphinx.stop
-      run "ln -nfs #{shared_path}/db/sphinx #{release_path}/db/sphinx"
-      thinking_sphinx.configure
-      #thinking_sphinx.start
-    end
-  end
   require 'thinking_sphinx/deploy/capistrano'
-  after 'deploy:update_code', 'deploy:new_sphinx'
-  after 'deploy:update_code', 'thinking_sphinx:start'
-  after 'deploy:setup',   'thininking_sphinx:setup'
+  if ENV["SPHINX"] == "yes"
+    namespace :deploy do
+      task :new_sphinx, :roles => [:app] do
+        thinking_sphinx.stop
+        run "ln -nfs #{shared_path}/db/sphinx #{release_path}/db/sphinx"
+        thinking_sphinx.configure
+        #thinking_sphinx.start
+      end
+    end
+    after 'deploy:update_code', 'deploy:new_sphinx'
+    after 'deploy:update_code', 'thinking_sphinx:start'
+    after 'deploy:setup',   'thininking_sphinx:setup'
+  else
+    namespace :deploy do
+      task :new_sphinx, :roles => [:app] do
+        run "ln -nfs #{shared_path}/db/sphinx #{release_path}/db/sphinx"
+        rake "thinking_sphinx:index"
+      end
+    end
+    after 'deploy:update_code', 'deploy:new_sphinx'
+  end
 end
